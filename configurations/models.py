c7a318265e2af3004480c9933cb1cd873e35e1c4
WesinAlves <wesinec@gmail.com>
2021-01-21 17:29:44 -0300

    add ordem servico

--- configurations/models.py
+++ configurations/models.py
@@ -21,15 +21,7 @@ class TipoAtividade(models.Model):
     # o nome TipoAtividade deveria ser alterado pois causa confusão nos relacionamentos
 
     def __str__(self):
-        return self.faix[:50] + '...'
-
-class GrupoAtividade(models.Model):
-    nom_grupo_atividade = models.CharField(max_length=200)
-    inativo = models.BooleanField()
-    atividades = models.ManyToManyField(Atividade)
-
-    def __str__(self):
-        return self.nom_grupo_atividade[:50] + '...'
+        return self.faixa[:50] + '...'
 
 class Unidade(models.Model):
     nome = models.CharField(max_length=200)
@@ -47,6 +39,17 @@ class TipoPacto(models.Model):
 
     def __str__(self):
         return self.desc_tipo_pacto[:50] + '...'
+
+        
+class GrupoAtividade(models.Model):
+    nom_grupo_atividade = models.CharField(max_length=200)
+    inativo = models.BooleanField()
+    tipo_pactos = models.ManyToManyField(TipoPacto)
+    atividades = models.ManyToManyField(Atividade)
+
+    def __str__(self):
+        return self.nom_grupo_atividade[:50] + '...'
+
 
 class Unidade_TipoPacto(models.Model):
     unidade = models.ForeignKey(Unidade, on_delete=models.CASCADE)
@@ -70,4 +73,44 @@ class UsuarioGrupolUnidade(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     excluido = models.BooleanField()
+
+class CriterioAvaliacao(models.Model):
+    descricao = models.CharField(max_length=100)
+    texto_explicativo = models.TextField(max_length=1000)
+    inativo = models.BooleanField()
+
+    def __str__(self):
+        return self.descricao[:50] + '...'
+
+class NotaAvaliacao(models.Model):
+    descricao = models.CharField(max_length=20)
+    avaliacao_simplificada = models.BooleanField()
+    avaliacao_detalhada = models.BooleanField()
+    limite_superior = models.FloatField()
+    limite_inferior = models.FloatField()
+    conceito = models.IntegerField(blank=True)
+
+    def __str__(self):
+        return self.descricao[:50] + '...'
+        
+class ItemAvaliacao(models.Model):
+    descricao = models.CharField(max_length=500)
+    impacto_nota = models.FloatField()
+    nota_maxima = models.ForeignKey(NotaAvaliacao, on_delete=models.CASCADE)
+    criterio_avaliacao = models.ForeignKey(CriterioAvaliacao, on_delete=models.CASCADE)
+    inativo = models.BooleanField()
+
+    def __str__(self):
+        return self.descricao[:50] + '...'
+
+class OrdemServico(models.Model):
+    descricao = models.TextField(max_length=1000)
+    data_inicio = models.DateField()
+    data_fim = models.DateField()
+    inativo = models.BooleanField()
+    grupo_atividades = models.ManyToManyField(GrupoAtividade)
+    criterio_avaliacoes = models.ManyToManyField(CriterioAvaliacao)
+
+    def __str__(self):
+        return self.descricao[:50]    
 
