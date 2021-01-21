0ed8a394c6eaeb47e507060fa67bfc6ecc761a23
WesinAlves <wesinec@gmail.com>
2021-01-21 09:06:29 -0300

    update models to manage

--- configurations/admin.py
+++ configurations/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Atividade, GrupoAtividade, TipoPacto
+from .models import Atividade, GrupoAtividade, TipoPacto, Usuario, Unidade
 
 admin.site.register(Atividade)
 admin.site.register(GrupoAtividade)
 admin.site.register(TipoPacto)
+admin.site.register(Usuario)
+admin.site.register(Unidade)
