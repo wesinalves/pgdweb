a3c2a1d1794e61dd2edef78611497a18afbfd4b5
WesinAlves <wesinec@gmail.com>
2021-01-25 18:05:55 -0300

    make migrations

--- plano/admin.py
+++ plano/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+
 # Register your models here.
+from .models import OrdemAtividade, OrdemTipoAtividade, OrdemGrupoAtividade, \
+    Pacto, Produto, Historico, Justificativa, Cronograma, \
+    OrdemCriterioAvaliacao, OrdemItemAvaliacao
+
+# Register your models here.
+admin.site.register(Pacto)
