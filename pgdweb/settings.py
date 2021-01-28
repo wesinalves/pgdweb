60fe13e0e620d2826fafdf044970022e77af422c
WesinAlves <wesinec@gmail.com>
2021-01-28 12:46:54 -0300

    add docker

--- pgdweb/settings.py
+++ pgdweb/settings.py
@@ -80,6 +80,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'postgres',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'postgres',
+    #     'HOST': 'db',
+    #     'PORT': 5432,
+    # }
 }
 
 
