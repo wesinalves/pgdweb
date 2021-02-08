dd06dadb90a56344c5d311230775abc729b9a9d2
WesinAlves <wesinec@gmail.com>
2021-02-08 16:50:22 -0300

    remove django crum

--- pgdweb/settings.py
+++ pgdweb/settings.py
@@ -49,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'crum.CurrentRequestUserMiddleware',
 ]
 
 ROOT_URLCONF = 'pgdweb.urls'
